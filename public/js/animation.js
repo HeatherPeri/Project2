24c84add670daf907b9456d7a4f31c43f70eb30c
yevheniia <jene4ka1818@gmail.com>
2019-05-20 11:50:31 -0400

    g

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,22 +1,29 @@
 $(function() {
-$('#home').hide()
-$('.bg1').hide()
-$('.navbar').hide()
-$('#introText').hide()
-$('.hero-btn').mouseenter(function(){
-    $('#introText').delay( 50 ).fadeIn(1000)
+//$('#home').hide()
+//$('.bg1').hide()
+//$('.navbar').hide()
+//$('#introText').hide()
+//$('.input-group').hide()
+//$('.hero-btn').mouseenter(function(){
+  //  $('#introText').delay( 50 ).fadeIn(1000)
 
-})
-$('.hero-btn').mouseleave(function(){
-  $('#introText').fadeOut()
-})
-$('.hero-btn').on('click', function(){
+//})
+//$('.hero-btn').mouseleave(function(){
+  //$('#introText').fadeOut()
+//})
+/*$('.hero-btn').on('click', function(){
     $('.hero-btn').remove();
     $('#introText').remove();
     $('#profile-hook').remove();
     $('.bg').fadeOut()
     $('.navbar').fadeIn(2000)
-    $('#home').fadeIn()
-$('.bg1').fadeIn()
+   
+    $('.input-group').fadeIn()*/
+
 })
-})
+
+
+
+
+
+
---
+++ public/js/search.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+    $(".input-group-text").click(function(){
+
+        var searchTerm = $(".form-control").val();// value entered by the user
+        //var newUrl = 'https://api.php?action=query&list=search'+searchTerm+'&srsearch=meaning'
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm + "&srsearch=meaning&format=json&callback=?"; // url to look for using the search input by the user
+    
+        $.ajax({
+            type:"GET",
+            url:url,
+            async:true,
+            dataType: "json",
+            success:function(data){
+                console.log(data[1][0]);
+                console.log(data[2][0]);
+                console.log(data[3][0]);
+            },
+    
+            error: function(errorMessage){alert("Error");}
+        }).then(function(response){
+            console.log(response)
+            $('#wikitext').append(response[2])
+        });
+    });
+  });
