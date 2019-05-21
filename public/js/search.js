aed3ad246e809e475cfc6f97f22d0cf3f3827c77
yevheniia <jene4ka1818@gmail.com>
2019-05-21 11:32:16 -0400

    moment_js_added

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //moment().format();
    /*var url = "https://api.nasa.gov/planetary/apod?api_key=DGayCeCopIiwsgjpM1jghFg2fFfzzpeXQZiI18IN";
 
 
@@ -30,11 +31,12 @@ $.ajax({
 
 
     //var newUrl = 'https://api.php?action=query&list=search'+searchTerm+'&srsearch=meaning'
-    $(".btn-outline-success").click(function(){
-
-        var searchTerm = $(".form-control").val();// value entered by the user
+    var todayDate = moment().format('YYYY-MM-DD');
+    var endDate = moment().add(7, 'days').format('YYYY-MM-DD'); 
+    console.log(todayDate)
+    var searchTerm = $(".form-control").val();// value entered by the user
         //var newUrl = 'https://api.php?action=query&list=search'+searchTerm+'&srsearch=meaning'
-        var url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DGayCeCopIiwsgjpM1jghFg2fFfzzpeXQZiI18IN'; // url to look for using the search input by the user
+        var url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date='+todayDate+'&end_date='+endDate+'&api_key=DGayCeCopIiwsgjpM1jghFg2fFfzzpeXQZiI18IN'; // url to look for using the search input by the user
     
         $.ajax({
           type:"GET",
@@ -46,14 +48,13 @@ $.ajax({
                 var try2 = result.near_earth_objects[element];
                 console.log(try2)
                 var theDate = try2[0].close_approach_data[0].close_approach_date
-                $('#firstP').append((theDate+ "\n <br>"))
+                $('#secondP').append((theDate+ "\n <br>"))
                 for (i = 0; i < try2.length; i++){
                   console.log(result.near_earth_objects[element])
                   console.log(try2[i])
-                  $('#firstP').append((try2[i].name + "\n <br>"))
+                  $('#secondP').append(('<p> name :</p>'+ '\n'+try2[i].name + "\n <br>"))
                 }
               });
           }
   });
 }) // url to look for using the search input by the user'
-})
