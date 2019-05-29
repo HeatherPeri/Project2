3b7f6c417e4c5b48abb4bf0076949b2fa4bda914
csvernon <47064599+csvernon@users.noreply.github.com>
2019-05-29 19:35:55 -0400

    Revert "Backanimation"

--- db/schema.sql
+++ db/schema.sql
@@ -2,15 +2,13 @@ CREATE DATABASE IF NOT EXISTS astrology_db;
 USE astrology_db;
 
 -- If the table already exists, remove it before trying to create the table again
-DROP TABLE IF EXISTS wikipedia;
+DROP TABLE IF EXISTS astro;
 
 -- Create the burgers table
-CREATE TABLE wikipedia (
+CREATE TABLE astro (
     id int NOT NULL AUTO_INCREMENT,
-    title varchar(255) NOT NULL,
-    description varchar(255) NOT NULL,
-    search_query varchar(255) NOT NULL,
+    astro_name varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-select * from wikipedia
+select * from astro
