98e53cfd7ca231a91201194bb25fc806b5e6da2a
csvernon <47064599+csvernon@users.noreply.github.com>
2019-05-29 19:20:38 -0400

    Revert "New updates"

--- models/index.js
+++ models/index.js
@@ -43,36 +43,23 @@ module.exports = db;
 
 
 
-// Dependencies
-// =============================================================
+// // Dependencies
+// // =============================================================
 
-// Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
+// // This may be confusing but here Sequelize (capital) references the standard library
+// var Sequelize = require("sequelize");
+// // sequelize (lowercase) references our connection to the DB.
+// var sequelize = require("../config/connection.js");
 
-// Creates a "Character" model that matches up with DB
-var Character = sequelize.define("character", {
-  // the routeName gets saved as a string
-  routeName: Sequelize.STRING,
-  // the name of the character (a string)
-  name: Sequelize.STRING,
-  // the character's role (a string)
-  role: Sequelize.STRING,
-  // the character's age (a string)
-  age: Sequelize.INTEGER,
-  
-  // and the character's force points (an int)
-  forcePoints: Sequelize.INTEGER
-}, {
-  // disable the modification of tablenames; By default, sequelize will automatically
-  // transform all passed model names (first parameter of define) into plural.
-  // if you don't want that, set the following
-  freezeTableName: true
-});
+// // Creates a "Chirp" model that matches up with DB
+// var Chirp = sequelize.define("chirp", {
+//   author: Sequelize.STRING,
+//   body: Sequelize.STRING,
+//   created_at: Sequelize.DATE
+// });
 
-// Syncs with DB
-Character.sync();
+// // Syncs with DB
+// Chirp.sync();
 
-// Makes the Character Model available for other files (will also create a table)
-module.exports = Character;
+// // Makes the Chirp Model available for other files (will also create a table)
+// module.exports = Chirp;
