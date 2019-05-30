d71d54ebcd269288d0539a56c9df07ccdbad2182
Heather Peri <heatherhperi@gmail.com>
2019-05-29 21:05:57 -0400

    calendar

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,17 @@
-CREATE DATABASE IF NOT EXISTS astrology_db;
+DROP DATABASE IF NOT EXISTS astrology_db;
+CREATE DATABASE astrology_db;
 USE astrology_db;
 
 -- If the table already exists, remove it before trying to create the table again
-DROP TABLE IF EXISTS astro;
+DROP TABLE IF EXISTS wikipedia;
 
 -- Create the burgers table
-CREATE TABLE astro (
+CREATE TABLE wikipedia (
     id int NOT NULL AUTO_INCREMENT,
-    astro_name varchar(255) NOT NULL,
+    title varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    search_query varchar(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-select * from astro
+select * from wikipedia
